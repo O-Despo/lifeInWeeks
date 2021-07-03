955d442c9c52169fbb87960e5e8803b06e8bfc06
Oliver Desposito <oliverdesposito@gmail.com>
2021-07-02 22:11:56 -0400

    added cascade effect

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
 $(function(){
-body = document.getElementById("dotFlex")
+    dotFlex = document.getElementById("dotFlex")
 
 weeksInYear = 4160
 weeksOver = 844
 
+$("#dotFlex").hide();
 
-for(let i = 0; i <= 4680; i++){
-    if (i % 52 == 0){
-        dot = document.createElement('week_dot_year')
+function drawDots(){
+    for(let i = 0; i <= weeksInYear; i++){
+        if (i % 52 == 0){
+            dot = document.createElement('week_dot_year')
+        }
+        else if (i <= weeksOver){
+            dot = document.createElement('week_dot_full')
+        }
+        else if (i == 4680){
+            dot = document.createElement('week_dot_death')
+        }
+        else{
+            dot = document.createElement('week_dot')
+        }
+        dot.classList.add("dot")
+        $(dot).hide()
+        dotFlex.appendChild(dot)        
     }
-    else if (i <= weeksOver){
-        dot = document.createElement('week_dot_full')
-    }
-    else if (i == 4680){
-        dot = document.createElement('week_dot_death')
-    }
-    else{
-        dot = document.createElement('week_dot')
-    }
-    body.appendChild(dot)
 }
 
-function introAnimate(){
-    introFlex = document.getElementById('large');
-    let opacity = 1.00;
-
-    for(let i = 0; i <= 100; i++){
-        opacity = opacity - 0.01
-        introFlex.style.opacity = opacity
-        console.log(opacity)
+function showDots(dot){  
+    dots = document.getElementsByClassName('dot')
+    for(let i = 0; i < dots.length; i++){
+        $(dots[i]).delay(i*1).fadeIn(500)
     }
-    // introFlex.style.opacity = opacity
 }
 
-});
+
+$("#intro-flex").click(function(){
+    $("#intro-flex").fadeOut("slow", function(){
+        $("#top-bar").fadeIn();
+        drawDots()
+        $("#dotFlex").show(function(){ 
+                showDots()
+            })
+    })
+})
+})
